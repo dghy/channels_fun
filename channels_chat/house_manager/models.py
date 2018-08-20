2913f3801473296e8b9acf1511e4b8a5967dfb9e
Daniel Tomaszuk <daniel.tomaszuk@netguru.pl>
2018-08-20 19:03:08 +0200

    Add event model and migrations with it.

--- channels_chat/house_manager/models.py
+++ channels_chat/house_manager/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+from chat.models import TimeStampModel
+
+
+class ScheduledEvent(TimeStampModel):
+    EVENT_TYPE = [
+        (1, 'Water Filters'),
+        (2, 'Empty Cesspool'),
+        (3, 'Other')
+    ]
+
+    description = models.CharField(max_length=255)
+    event_type = models.SmallIntegerField(choices=EVENT_TYPE)
+    color = models.CharField(max_length=10)
